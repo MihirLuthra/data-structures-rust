8c9dc1f0dae13e31fd3dd2389575756bb51f2826
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-09-15 21:45:12 +0530

    generic linked list node

---
+++ src/linked_list/node.rs
@@ -0,0 +1,12 @@
+pub type Next<T> = Box<Option<Node<T>>>;
+
+pub struct Node<T> {
+    pub data: T,
+    pub next: Next<T>,
+}
+
+impl<T> Node<T> {
+    pub fn new(data: T) -> Self {
+        Node { data, next: Box::new(None) }
+    }
+}
