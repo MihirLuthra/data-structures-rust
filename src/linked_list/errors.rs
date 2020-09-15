ed64b98e8a383e0dcc8dda54c865ea167cb67ad8
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-09-15 21:45:28 +0530

    linked list errors

---
+++ src/linked_list/errors.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub enum Error {
+    PositionOutOfBounds(usize, usize),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+        match self {
+            Error::PositionOutOfBounds(posn, length) => 
+                write!(f, "Trying to insert at {} but linked list length is {}", posn, length),
+        }
+    }
+}
+impl std::error::Error for Error {}
